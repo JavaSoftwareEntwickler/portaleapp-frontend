a9492c4785ecb89721cc935e9082e696d83aa083
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-03-06 10:07:51 +0100

    Implementata chiamata HTTP mock per il pagamento con dati hardcoded per il test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule  } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+/** Definisco il componente standalone con la logica per fare la chiamata HTTP. */
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports:[CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true  // Aggiungi questa riga
 })
 export class AppComponent {
   title = 'portaleapp-frontend';
+  messaggio: string | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  processaPagamento(): void {
+    const pagamentoData = {
+      tipoPagamento: 'pagamentocartacredito',
+      tipoSconto: 'scontofisso',
+      prezzo: 100
+    };
+
+    this.http.post('http://localhost:8080/pagamento/processa', pagamentoData)
+      .subscribe(
+        (response: any) => {
+          this.messaggio = response.messaggio;
+        },
+        (error) => {
+          console.error('Errore nel pagamento:', error);
+        }
+      );
+  }
 }
