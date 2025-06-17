8e6d43a6b5a037db7b5ad478ff074ca0e18f68f2
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-06-17 12:09:14 +0200

    input per BE dinamico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule  } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 /** Definisco il componente standalone con la logica per fare la chiamata HTTP. */
 
 @Component({
   selector: 'app-root',
-  imports:[CommonModule, HttpClientModule],
+  imports:[CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone: true  // Aggiungi questa riga
+  standalone: true  
 })
 export class AppComponent {
   title = 'portaleapp-frontend';
   messaggio: string | undefined;
+  // Variabili legate ai campi del form
+  tipoPagamento = 'pagamentocartacredito';
+  tipoSconto = 'nessunosconto';
+  prezzo = 100;
 
   constructor(private http: HttpClient) {}
 
   processaPagamento(): void {
     const pagamentoData = {
-      tipoPagamento: 'pagamentocartacredito',
-      tipoSconto: 'scontofisso',
-      prezzo: 100
+      tipoPagamento: this.tipoPagamento,
+      tipoSconto: this.tipoSconto,
+      prezzo: this.prezzo
     };
 
     this.http.post('http://localhost:8080/pagamento/processa', pagamentoData, { responseType: 'text' })
