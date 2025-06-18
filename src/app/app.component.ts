e51c3cd07f6fcb20d90b20195b71cb0feaee06a4
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-06-18 16:55:34 +0200

    nav bar e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagamentoComponent } from './pagamento/pagamento.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports:[CommonModule, PagamentoComponent],
+  imports:[CommonModule, PagamentoComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true  
