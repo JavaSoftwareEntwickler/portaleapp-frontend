a1a4bde58a415d88cbda221e44b248bec49ffb58
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-06-18 08:02:14 +0200

    aggiunto animazione a response del BE

--- src/app/pagamento/pagamento.component.ts
+++ src/app/pagamento/pagamento.component.ts
@@ -2,19 +2,32 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PagamentoService } from './pagamento.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-pagamento',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './pagamento.component.html',
-  styleUrls: ['./pagamento.component.css']
+  styleUrls: ['./pagamento.component.css'],
+    animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ opacity: 0 })),
+      ]),
+    ])
+  ]
 })
 export class PagamentoComponent {
   messaggio: string | undefined;
   tipoPagamento = 'pagamentocartacredito';
   tipoSconto = 'nessunosconto';
   prezzo = 100;
+  mostraMessaggio = false;
 
   constructor(private pagamentoService: PagamentoService) {}
 
@@ -29,5 +42,14 @@ export class PagamentoComponent {
       next: (response) => this.messaggio = response,
       error: (err) => console.error('Errore nel pagamento: ', err)
     });
+    this.messaggio = `Pagamento ${this.tipoPagamento} - Sconto ${this.tipoSconto} - €${this.prezzo}`;
+    this.mostraMessaggio = true;
+
+    setTimeout(() => {
+      this.mostraMessaggio = false;
+      // Facoltativo: azzera il testo dopo la dissolvenza
+      setTimeout(() => this.messaggio = undefined, 500);
+    }, 4000);
   }
 }
+
