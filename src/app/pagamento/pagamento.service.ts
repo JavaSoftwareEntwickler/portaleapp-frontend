0bc18c7c87cf5b99d3d62a29397dffc783f19550
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-10-29 16:27:43 +0100

    eliminata la porta dalle chiamate BE per produzione

--- src/app/pagamento/pagamento.service.ts
+++ src/app/pagamento/pagamento.service.ts
@@ -7,7 +7,8 @@ export class PagamentoService {
   private readonly http = inject(HttpClient);
   
   processaPagamento(data : any): Observable<string>{
-    return this.http.post('http://localhost:8080/pagamento/processa', data,{
+    //return this.http.post('http://localhost:8080/pagamento/processa', data,{
+    return this.http.post('http://localhost/pagamento/processa', data,{
       responseType : 'text'
     });
   }
